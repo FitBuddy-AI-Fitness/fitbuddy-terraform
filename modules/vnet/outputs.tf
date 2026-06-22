5c6458951ccfff9e05786ee03eb142bfc0eb637f
YourGitHubUsername <YourEmail@example.com>
2026-06-22 14:59:16 +0530

    fix(vnet): update outputs.tf to match renamed appgw pip resource

--- modules/vnet/outputs.tf
+++ modules/vnet/outputs.tf
@@ -2,4 +2,4 @@ output "hub_vnet_id" { value = azurerm_virtual_network.hub.id }
 output "spoke_vnet_id" { value = azurerm_virtual_network.spoke.id }
 output "aks_subnet_id" { value = azurerm_subnet.aks.id }
 output "pe_subnet_id" { value = azurerm_subnet.private_endpoints.id }
-output "appgw_public_ip" { value = azurerm_public_ip.appgw_pip.ip_address }
+output "appgw_public_ip" { value = azurerm_public_ip.appgw.ip_address }
