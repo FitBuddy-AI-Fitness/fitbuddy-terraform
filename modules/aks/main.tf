3139040b57526b449812fea9867fedd46799f4c9
aravindroy1 <127665017+aravindroy1@users.noreply.github.com>
2026-06-26 02:50:28 +0530

    Update main.tf

--- modules/aks/main.tf
+++ modules/aks/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   network_profile {
     network_plugin    = "azure"
     load_balancer_sku = "standard"
-    service_cidr      = "172.16.0.0/16"
+    service_cidr      = "172.16.0.0/16" 
     dns_service_ip    = "172.16.0.10"
   }
 
