6b097084895481f6cd52d117154d8e0c0be4e7ae
aravindroy1 <127665017+aravindroy1@users.noreply.github.com>
2026-06-26 03:11:29 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "Aravind-RG-test"  
     storage_account_name = "fitbuddytfstate"  
-    container_name       = "tfstate"
+    container_name       = "tfstate" 
     key                  = "terraform.tfstate"  
   }
 }
